8cf5c4b06fc1e3b5d02450a8895516258cde6eb8
Alex Navarro <wosorio@hightech-corp.com>
2021-07-02 16:01:02 -0600

    defining action types

---
+++ src/application/types/index.js
@@ -0,0 +1,10 @@
+export const CREATE_PROJECT = '@project/CREATE_PROJECT';
+
+export const LOGIN_SUCCESS = '@auth/LOGIN_SUCCESS';
+export const LOGIN_ERROR = '@auth/LOGIN_ERROR';
+
+export const SIGNOUT_SUCCESS = '@auth/SIGNOUT_SUCCESS';
+export const SIGNOUT_ERROR = '@auth/SIGNOUT_ERROR';
+
+export const SIGNUP_SUCCESS = '@auth/SIGNUP_SUCCESS';
+export const SIGNUP_ERROR = '@auth/SIGNUP_ERROR';
