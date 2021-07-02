c24ea2fea81415445eb839bd4d791d5386382c6c
Alex Navarro <wosorio@hightech-corp.com>
2021-07-02 15:09:50 -0600

    configuring redux and firebase

--- src/index.js
+++ src/index.js
@@ -4,11 +4,47 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider, useSelector } from 'react-redux';
+import firebase from 'firebase/app';
+import { ReactReduxFirebaseProvider, isLoaded } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+
+import store from './application/store/store';
+import { rrfConfig } from './application/config/fbConfig';
+
+const rrfProps = {
+	firebase,
+	config: rrfConfig,
+	dispatch: store.dispatch,
+	createFirestoreInstance
+}
+
+const AuthIsLoaded = ({ children }) => {
+	
+	const { auth } = useSelector(state => state.firebase)
+	
+	if (!isLoaded(auth)) {
+		return (
+			<div className="progress">
+				<div className="indeterminate"></div>
+			</div>
+		)
+	}
+
+	return children
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.Fragment>
+		<Provider store={store}>
+			<ReactReduxFirebaseProvider {...rrfProps}>
+				<AuthIsLoaded>
+					<App />
+				</AuthIsLoaded>
+			</ReactReduxFirebaseProvider>
+		</Provider>
+	</React.Fragment>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
